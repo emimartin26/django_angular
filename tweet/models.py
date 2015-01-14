1011f5e1fcf8e06289b718b1f25880eda20b2dfa
emimartin <emimartin26@gmail.com>
2015-01-14 13:56:10 -0300

    Cree app tweet

---
+++ tweet/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Tweet(models.Model):
+    mensaje = models.TextField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignObject(User)
